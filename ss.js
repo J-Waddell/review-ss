190e772819bac8d0ef0c1a8ad937e80f7738a47d
Jesse Waddell <waddelljesse1@gmail.com>
2018-05-24 09:53:52 -0500

    w/o json population

--- ss.js
+++ ss.js
@@ -1,4 +1,11 @@
-var doc = DocumentApp.openById('1wNmcemKDDY2BXfKVV0XMoIVBgV7p73mhOQGWjgu65aQ');
-  function googapp(doc) {
-    return doc;
-  }
+var data ='[{"title": " ", "Stress - Relax - Relaxation / Sleepy / Energy / Taste-Flavor / Love - Awsome - Great / Focus - Clarity /": " ", "blend": " ", "reviewer": " "}]'
+
+var json = JSON.parse(data);
+
+console.log(json);
+console.log(json[0]);
+console.log(json[0].title);
+
+// success: function (data) {
+//     var json = JSON.parse(data);
+// }
